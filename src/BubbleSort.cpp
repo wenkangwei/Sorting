23beebfae5164a12bade98942173801829d5fdd7
wenkangwei <wenkanw@g.clemson.edu>
2020-08-24 15:55:19 -0400

    Update BubbleSort.cpp

--- src/BubbleSort.cpp
+++ src/BubbleSort.cpp
@@ -5,7 +5,7 @@ void bubble_Sort(int *arr, int len)
 	int temp;
 	for (int i = 0; i < len; ++i)
 	{
-		for (int j = 0; j < len-1; ++j)
+		for (int j = 0; j < len-1-i; ++j)
 		{
 			if (arr[j]>=arr[j+1])
 			{
